597c92782abfc3a1637e77c03b2101c9ececbddd
jad amine <jade1992amine@gmail.com>
2022-08-19 18:17:05 +0300

    Added Routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:world_time_app/screens/choose_location.dart';
 import 'package:world_time_app/screens/home.dart';
+import 'package:world_time_app/screens/loading.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Home(),
+  runApp(MaterialApp(
+    initialRoute: '/home',
+    routes: {
+      '/': (context) => const Loading(),
+      '/home': (context) => const Home(),
+      '/location': (context) => const ChooseLocation(),
+    },
   ));
 }
 
