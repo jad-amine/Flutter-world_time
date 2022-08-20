4e9054c6c1b18d34c66069434a6385466db7c6ec
jad amine <jade1992amine@gmail.com>
2022-08-20 17:47:03 +0300

    Created World Time service

--- lib/screens/loading.dart
+++ lib/screens/loading.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-import 'package:http/http.dart';
 import 'package:flutter/material.dart';
 
 class Loading extends StatefulWidget {
@@ -10,18 +8,6 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-
-  // Fetch data from the 3rd party API
-  void getTime() async{
-    Response response = await get(Uri.parse('http://worldtimeapi.org/api/timezone/Europe/London'));
-    Map data = jsonDecode(response.body);
-
-    // get properties from data
-    String datetime = data['datetime'];
-    String offset = data['utc_offset'];
-    print(datetime);
-    print(offset);
-  }
 
   @override
   void initState() {
