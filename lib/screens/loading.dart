9edf1abe28ba0e00647ffbd962fe447b86fa562a
jad amine <jade1992amine@gmail.com>
2022-08-20 14:27:14 +0300

    Added HTTP request to fetch London datetime

--- lib/screens/loading.dart
+++ lib/screens/loading.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 import 'package:http/http.dart';
+import 'package:flutter/material.dart';
 
 class Loading extends StatefulWidget {
   const Loading({Key? key}) : super(key: key);
@@ -11,13 +12,21 @@ class Loading extends StatefulWidget {
 class _LoadingState extends State<Loading> {
 
   // Fetch data from the 3rd party API
-  void getData() async{
+  void getTime() async{
+    Response response = await get(Uri.parse('http://worldtimeapi.org/api/timezone/Europe/London'));
+    Map data = jsonDecode(response.body);
 
+    // get properties from data
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'];
+    print(datetime);
+    print(offset);
   }
 
   @override
   void initState() {
     super.initState();
+    getTime();
   }
 
   @override
