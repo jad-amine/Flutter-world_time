e515460b005d0cdd6c3e692a8d4426dd2539ead2
jad amine <jade1992amine@gmail.com>
2022-08-22 17:44:20 +0300

    Added UI widgets to show time

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -13,22 +13,43 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    final data = ModalRoute.of(context)!.settings.arguments;
-    print(data);
+    final data = ModalRoute.of(context)!.settings.arguments as Map;
 
     return Scaffold(
       body: SafeArea(
-          child: Column(
-        children: [
-          TextButton.icon(
-            onPressed: (){
-              Navigator.pushNamed(context, '/location');
-            },
-            icon: const Icon(Icons.edit_location),
-            label: const Text('Edit Location'),
-          )
-        ],
-      )),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(0,120,0,0),
+            child: Column(
+              children: [
+                TextButton.icon(
+                  onPressed: (){
+                    Navigator.pushNamed(context, '/location');
+                  },
+                  icon: const Icon(Icons.edit_location),
+                  label: const Text('Edit Location'),
+                ),
+                const SizedBox(height: 20,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(data['location'],
+                    style: const TextStyle(
+                      fontSize: 28,
+                      letterSpacing: 2,
+                    ),),
+                  ],
+                ),
+                const SizedBox(height: 20,),
+                Text(
+                  data['time'],
+                  style: const TextStyle(
+                    fontSize: 66
+                  ),
+                )
+              ],
+            ),
+          ),
+      ),
     );
   }
 }
