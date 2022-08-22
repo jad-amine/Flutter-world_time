91f435b1522f60b148b48eacbc8665d7e2386f06
jad amine <jade1992amine@gmail.com>
2022-08-22 17:27:50 +0300

    Added intl package

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart';
+import 'package:intl/intl.dart';
 
 class WorldTime{
 
@@ -24,7 +25,7 @@ class WorldTime{
     DateTime now = DateTime.parse(datetime);
     now = now.add(Duration(hours: int.parse(offset)));
     // set the time property
-    time = now.toString();
+    time = DateFormat.jm().format(now);
     } catch (e) {
       print(e);
       time = "could not get time data";
