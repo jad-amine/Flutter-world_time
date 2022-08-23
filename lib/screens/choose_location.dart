f87945bb579443ae5c9edcab89bd2a753ceed8e0
jad amine <jade1992amine@gmail.com>
2022-08-23 07:05:39 +0300

    Created country Card

--- lib/screens/choose_location.dart
+++ lib/screens/choose_location.dart
@@ -34,10 +34,18 @@ class _ChooseLocationState extends State<ChooseLocation> {
       body: ListView.builder(
         itemCount: locations.length,
         itemBuilder: (context, index){
-          return Card(
-            child: ListTile(
-              onTap: (){},
-              title: Text(locations[index].location),
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 1, horizontal: 4),
+            child: Card(
+              child: ListTile(
+                onTap: (){
+
+                },
+                title: Text(locations[index].location),
+                leading: CircleAvatar(
+                  backgroundImage: AssetImage('assets/${locations[index].flag}'),
+                ),
+              ),
             ),
           );
         },
